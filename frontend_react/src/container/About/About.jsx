4228c7dc3ac515e56bd2641b8cb23e1f96567145
Jake Young <Jake_Young@dell.com>
2022-11-15 23:08:11 +0000

    Adding sanity and wrapper

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,32 +1,19 @@
 import React, {useState, useEffect } from 'react'
 import {motion} from 'framer-motion';
-import {images} from '../../constants'
+import {urlFor, client} from '../../client';
+import { AppWrap } from '../../wrapper';
 import './About.scss'
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good web designer',
-    imgUrl: images.about02
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am a good web UI/UX',
-    imgUrl: images.about03
-  },
-  {
-    title: 'Web Animations',
-    description: 'I am a good web animator',
-    imgUrl: images.about04
-  }
-  ];
-
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect (() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -42,7 +29,7 @@ const About = () => {
               className="app__profiles-item"
               key={about.title + index}
               >
-                <img src={about.imgUrl} alt={about.title} />
+                <img src={urlFor(about.imgUrl)} alt={about.title} />
                 <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
                 <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
               </motion.div>
@@ -52,4 +39,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about')
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import './Header.scss'
-import { motion } from 'framer-motion'
-import { images } from '../../constants'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
 
 const scaleVariants = {
   whileInView: {
@@ -9,14 +11,13 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut'
-    }  
-  }
-}
+      ease: 'easeInOut',
+    },
+  },
+};
 
-const Header = () => {
-  return (
-    <div className="app__header app__flex">
+const Header = () => (
+  <div className="app__header app__flex">
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -27,12 +28,12 @@ const Header = () => {
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Jake</h1>
+            <h1 className="head-text">Micael</h1>
           </div>
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Software Engineer</p>
+          <p className="p-text">Web Developer</p>
           <p className="p-text">Freelancer</p>
         </div>
       </div>
@@ -43,7 +44,7 @@ const Header = () => {
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+      <img src={images.oldProfile} alt="profile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
@@ -65,7 +66,6 @@ const Header = () => {
       ))}
     </motion.div>
   </div>
-  )
-}
+);
 
-export default Header
+export default AppWrap(Header, 'home');
