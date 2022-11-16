716ee6a8634d704677206380234d1ae8db043266
Jake Young <Jake_Young@dell.com>
2022-11-16 23:15:30 +0000

    Adding work section

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { About, Footer, Header, Skills, Testimonial, Work } from './container'
-import { Navbar } from './components'
-import './App.scss'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div className='app'>
-        <Navbar />
-        <Header />
-        <About />
-        <Work />
-        <Skills />
-        <Testimonial />
-        <Footer />
-    </div>
-  )
-}
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
-export default App
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
+
+export default App;
